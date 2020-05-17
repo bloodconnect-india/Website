96942cf6dbbffe415578de40ce786b711305cc2b
Anish <hney1000.ay1@gmail.com>
2020-05-17 17:59:51 +0530

    add carousel in homepage and changed testimonial

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,39 @@
-$("#myCarousel").on("slide.bs.carousel", function(e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $(".carousel-item").length;
-  
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(".carousel-item")
-            .eq(i)
-            .appendTo(".carousel-inner");
-        } else {
-          $(".carousel-item")
-            .eq(0)
-            .appendTo($(this).find(".carousel-inner"));
-        }
+$(".slider").slick({
+
+  // normal options...
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  loop: true,  
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
       }
-    }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        dots: true
+      }
+
+      }, {
+
+      breakpoint: 280,
+      settings: "unslick" // destroys slick
+
+    }]
   });
-  
