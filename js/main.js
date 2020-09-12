ef151252977cb00fd5469a8df05f7db2eed94c04
Anish Yadav <hney1000.ay1@gmail.com>
2020-09-12 20:38:07 +0530

    added plasma donation option in navbar

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
 $('.carousel').carousel()
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // Get the navbar
 var navbarSticky = document.getElementById("navbar-sticky");
 var navbar = document.getElementById("navbar")
 var navbarToggler = document.getElementById("mobile-nav-toggler")
-var navbarSm  = document.getElementById("navbar-sm")
+var navbarSm = document.getElementById("navbar-sm")
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= window.innerHeight-100) {
+if (window.pageYOffset < 50 && (window.location.href.includes("index") || window.location.href.includes("donor"))) {
+    navbar.classList.add("show-banner")
+    document.getElementById("mobile-nav").classList.add("show-banner")
+  }
+  else if (window.pageYOffset >= window.innerHeight - 100) {
     navbarSticky.classList.remove('hide')
     navbarSticky.classList.add('show-flex')
     navbar.classList.add('hide')
+    navbar.classList.remove('show-banner')
     navbarToggler.classList.remove('color-white')
     navbarToggler.classList.add('color-dark')
+    document.getElementById("mobile-nav").classList.remove("show-banner")
   } else {
     navbarSticky.classList.add('hide')
     navbarSticky.classList.remove('show-flex')
     navbar.classList.remove('hide')
-    if(!navbarToggler.classList.contains('color-dark-fixed')){
-    navbarToggler.classList.add('color-white')
-    navbarToggler.classList.remove('color-dark')
+    navbar.classList.remove('show-banner')
+    document.getElementById("mobile-nav").classList.remove("show-banner")
+    if (!navbarToggler.classList.contains('color-dark-fixed')) {
+      navbarToggler.classList.add('color-white')
+      navbarToggler.classList.remove('color-dark')
     }
   }
   // if (window.pageYOffset >= stickySm) {
@@ -32,35 +40,35 @@ function myFunction() {
   // } else {
   //   navbarSm.classList.remove("sticky-sm");
   // }
-} 
+}
 
 
 
 toggleMobileNav = () => {
   var mobileNav = document.getElementById("mobile-nav")
   var mobileNavList = document.getElementById("mobile-nav-list")
-  if(mobileNavList.classList.contains("hide")){
+  if (mobileNavList.classList.contains("hide")) {
     mobileNavList.classList.remove("hide")
     mobileNavList.classList.add("show")
     navbarToggler.classList.add("mr-0")
     mobileNav.classList.add("w-100")
     navbarToggler.innerHTML = "<i class='fa fa-times color-main f-20' ></i>"
-  }else{
+  } else {
     mobileNavList.classList.remove("show")
     mobileNavList.classList.add("hide")
     navbarToggler.classList.remove("mr-0")
     mobileNav.classList.remove("w-100")
     navbarToggler.innerHTML = "<i class='fa fa-bars f-20' ></i>"
   }
-     
+
 }
 
 
 var hashParams = window.location.hash.substr(1).split('&');
-for(var i = 0; i < hashParams.length; i++){
-    var p = hashParams[i].split('=');
-    if(document.getElementById(p[0].toLowerCase()))
-      document.getElementById(p[0].toLowerCase()).value = decodeURIComponent(p[1]);
+for (var i = 0; i < hashParams.length; i++) {
+  var p = hashParams[i].split('=');
+  if (document.getElementById(p[0].toLowerCase()))
+    document.getElementById(p[0].toLowerCase()).value = decodeURIComponent(p[1]);
 }
 
 
@@ -68,7 +76,7 @@ for(var i = 0; i < hashParams.length; i++){
 
 goToTeam = () => {
   window.scrollTo({
-    top:$("#boardMembers").offset().top-window.innerHeight/2,
+    top: $("#boardMembers").offset().top - window.innerHeight / 2,
     behavior: 'smooth'
   })
 }
